5a6309e17bedb4c6090187c9aeef7b62d0b45d31
Federico Alegría <116465926+fedaalgo@users.noreply.github.com>
2026-01-18 16:23:25 -0600

    tt2541.R

---
+++ 2025/41/tt2541.R
@@ -0,0 +1,56 @@
+# --- tidytuesday::2541 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-10-14/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  styler, # https://cran.r-project.org/web/packages/styler/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+df <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-10-14/food_security.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-10-14/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# visualise ----
+
+df |> 
+  filter(area == "El Salvador") |> 
+  filter(item == "Prevalence of obesity in the adult population (18 years and older) (percent)") |> 
+  ggplot(aes(x = year_end, y = value)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    x = "date",
+    y = "count",
+    title = "prevalence of obesity in the adult population",
+    subtitle = "El Salvador"
+  ) +
+  theme_minimal()
+
+# ...

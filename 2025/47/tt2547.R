784bf5293f528e15a7ae9a7992909e4ec986ab60
Federico Alegría <116465926+fedaalgo@users.noreply.github.com>
2026-01-24 19:32:08 -0600

    create tt2547.R

---
+++ 2025/47/tt2547.R
@@ -0,0 +1,48 @@
+# --- tidytuesday::2547 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2025/2025-11-25/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  styler, # https://cran.r-project.org/web/packages/styler/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+df <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-11-25/spi_indicators.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2025/2025-11-25/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# visualise ----
+
+df |> 
+  filter(country == "El Salvador") |> 
+  ggplot(aes(x = year, y = population)) +
+  geom_line() + 
+  theme_minimal()
+
+# ...

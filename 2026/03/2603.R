eec66452d909f02a09d5fc8f305baff21295a85c
Federico Alegría <116465926+fedaalgo@users.noreply.github.com>
2026-02-09 16:34:11 -0600

    update 2603.R

--- 2026/03/2603.R
+++ 2026/03/2603.R
@@ -10,10 +10,15 @@
 pacman::p_load(
   data.table, # https://cran.r-project.org/web/packages/data.table/
   janitor, # https://cran.r-project.org/web/packages/janitor/
+  ggraph, # https://cran.r-project.org/web/packages/ggraph/
+  igraph, # https://cran.r-project.org/web/packages/igraph/
+  likert, # https://cran.r-project.org/web/packages/likert/
   skimr, # https://cran.r-project.org/web/packages/skimr/
   styler, # https://cran.r-project.org/web/packages/styler/
+  stopwords, # https://cran.r-project.org/web/packages/stopwords/
   tidytext, # https://cran.r-project.org/web/packages/tidytext/
-  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+  tidyverse, # https://cran.r-project.org/web/packages/tidyverse/
+  wordcloud2 # https://cran.r-project.org/web/packages/wordcloud2/
 )
 
 # import
@@ -45,12 +50,85 @@ df |>
   summarise(n = n()) |>
   arrange(desc(n))
 
-# transform ----
+stopwords_en <-
+  data.frame(word = stopwords(language = "en")) |>
+  rbind(data.frame(
+    word = c(
+      "apod.nasa.gov",
+      "also",
+      "digg_skin",
+      "digg_un",
+      "html",
+      "http",
+      "known"
+    )
+  )
+)
+
+# sample
+set.seed(123)
+
+dfs <- df |> 
+  slice_sample(n = 100)
 
 # visualise ----
 
-# model ----
+dfs |> 
+  select(explanation) |> 
+  unnest_ngrams(word, explanation, n = 2) |> 
+  group_by(word) |> 
+  summarise(n = n()) |> 
+  arrange(desc(n)) |> 
+  filter(str_length(word) > 5)
 
-# communicate ----
+dfs_bigrams <- 
+  dfs |> 
+  select(explanation) |> 
+  filter(!is.na(explanation)) |>
+  filter(str_trim(explanation) != "") |>
+  unnest_ngrams(word, explanation, n = 2)
 
-# ...
+dfs_bigrams_sep <- 
+  dfs_bigrams |> 
+  separate(word, c("word1", "word2"), sep = " ") |>
+  filter(!is.na(word1) & !is.na(word2))
+
+dfs_bigrams_filtered <- dfs_bigrams_sep |>
+  filter(!word1 %in% stopwords_en$word) |>
+  filter(!word2 %in% stopwords_en$word)
+
+dfs_bigram_counts <- dfs_bigrams_filtered |> 
+  count(word1, word2, sort = TRUE)
+
+## bi-gram graph
+dfs_bigram_graph <- dfs_bigram_counts |> 
+  filter(n >= 5) |> 
+  graph_from_data_frame()
+
+dfs_bigram_graph
+
+a <- grid::arrow(type = 'closed', length = unit(.15, 'inches'))
+
+set.seed(8080)
+
+ggraph(dfs_bigram_graph, layout = "fr") +
+  geom_edge_link(
+    aes(edge_alpha = n),
+    show.legend = FALSE,
+    arrow = a,
+    end_cap = circle(.07, 'inches')
+  ) +
+  geom_node_point(color = "#000000",
+                  alpha = 0.85,
+                  size = 3) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+  labs(
+    title = "APOD bi-gram",
+    subtitle = " ",
+    caption = "arrows' saturation show level of recurrence"
+  ) +
+  theme(text = element_text(family = 'Consolas'),
+        plot.title = element_text(face = 'bold')
+)
+
+ggsave("apod_bi-gram.png", width = 12, height = 8, dpi = 300)

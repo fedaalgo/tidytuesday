d9a9d241a1c5ae931704fb992154909227bf0319
Federico Alegría <116465926+fedaalgo@users.noreply.github.com>
2026-04-21 16:57:32 -0600

    create 2615.R

---
+++ 2026/15/2615.R
@@ -0,0 +1,73 @@
+# --- tidytuesday::2615 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2026/2026-04-14/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.5", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+dfa <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-14/beaufort_scale.csv'
+  ) |>
+  clean_names()
+
+dfb <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-14/birds.csv'
+  ) |>
+  clean_names()
+
+dfc <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-14/sea_states.csv'
+  ) |>
+  clean_names()
+
+dfd <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-14/ships.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://github.com/rfordatascience/tidytuesday/raw/refs/heads/main/data/2026/2026-04-14/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# tokenize
+# df |>
+#   unnest_tokens(output = word, input = variable) |>
+#   anti_join(stop_words, by = "word") |>
+#   group_by(word) |>
+#   summarise(n = n()) |>
+#   arrange(desc(n))
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# communicate ----
+
+# ...

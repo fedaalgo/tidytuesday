fb1125a503b56feee8f4a37f4455975e2618b422
Federico Alegría <116465926+fedaalgo@users.noreply.github.com>
2026-05-03 16:18:56 -0600

    create 2617.R

---
+++ 2026/17/2617.R
@@ -0,0 +1,65 @@
+# --- tidytuesday::2617 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2026/2026-04-28/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.6", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+dfa <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-28/agreements.csv'
+  ) |>
+  clean_names()
+
+dfb <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-28/quantity_codes.csv'
+  ) |>
+  clean_names()
+
+dfc <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-28/tariff_agricultural.csv'
+  ) |>
+  clean_names()
+
+dfd <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-04-28/tariff_codes.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2026/2026-04-28/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# transform ----
+
+# visualise ----
+
+# model ----
+
+# communicate ----
+
+# ...

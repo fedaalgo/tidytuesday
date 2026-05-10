ce357c5ac2708790c88b84a6ae038a9f9f6632a0
Federico Alegría <116465926+fedaalgo@users.noreply.github.com>
2026-05-10 17:21:46 -0600

    create 2618.R

---
+++ 2026/18/2618.R
@@ -0,0 +1,63 @@
+# --- tidytuesday::2618 --- #
+# https://github.com/rfordatascience/tidytuesday/blob/main/data/2026/2026-05-05/readme.md
+
+# setup ----
+
+# library path
+.libPaths(c("~/.local/share/R/x86_64-pc-linux-gnu-library/4.6", .libPaths()))
+
+# packages
+pacman::p_load(
+  data.table, # https://cran.r-project.org/web/packages/data.table/
+  janitor, # https://cran.r-project.org/web/packages/janitor/
+  skimr, # https://cran.r-project.org/web/packages/skimr/
+  tidytext, # https://cran.r-project.org/web/packages/tidytext/
+  tidyverse # https://cran.r-project.org/web/packages/tidyverse/
+)
+
+# import
+dfa <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-05-05/food_beverages.csv'
+  ) |>
+  clean_names()
+
+dfb <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-05-05/textiles.csv'
+  ) |>
+  clean_names()
+
+dfc <-
+  fread(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-05-05/transport.csv'
+  ) |>
+  clean_names()
+# dictionary
+# https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/main/data/2026/2026-05-05/readme.md
+
+# understand ----
+
+# names
+df |> 
+  slice(0) |> 
+  glimpse()
+
+# glimpse & skim
+df |>
+  glimpse() |>
+  skim()
+
+# transform ----
+
+# visualise ----
+
+dfa |> 
+  ggplot(aes(x = year, y = beer)) +
+  geom_line()
+
+# model ----
+
+# communicate ----
+
+# ...

f30329e121b38249780bf0c8899ee178e3960529
Federico Alegría <116465926+fedaalgo@users.noreply.github.com>
2026-06-02 19:10:23 -0600

    update 2621.R

--- 2026/21/2621.R
+++ 2026/21/2621.R
@@ -44,4 +44,17 @@ df |>
 
 # communicate ----
 
-# ...
+# ... 
+
+# The “Sustainable Energy for all (SE4ALL)” initiative, launched in 2010 by
+# the UN Secretary General, established three global objectives to be
+# accomplished by 2030: to ensure universal access to modern energy services, to
+# double the global rate of improvement in global energy efficiency, and to
+# double the share of renewable energy in the global energy mix. SE4ALL database
+# supports this initiative and provides country level historical data for access
+# to electricity and non-solid fuel; share of renewable energy in total final
+# energy consumption by technology; and energy intensity rate of improvement.
+
+# - which countries have the lowest capacity for solar energy?
+# - what form of renewable energy has, on average, experienced the fasted rate
+#   of adoption?
